b9ed17f6039c0e57b5121a40e69de6fbf909f60c
korezawa1005 <t.k.oshihiki@gmail.com>
2025-12-05 00:00:25 +0900

    python60

--- practice.py
+++ practice.py
@@ -25,3 +25,23 @@ def swap_colon(str1):
     return right + ":" + left
 
 print(swap_colon("hello:world") == "world:hello")
+
+def atgc_count(str_atgc, str_bpname):
+    result = str_atgc.count(str_bpname)
+    return result
+
+print(atgc_count('AAGCCCCATGGTAA', 'A') == 5)
+
+def check_lower(str_engsentences):
+    return str_engsentences.islower()
+
+print(check_lower('down down down') == True)
+print(check_lower('There were doors all round the hall, but they were all locked') == False)
+
+def remove_clause(str_engsentences):
+    pos = str_engsentences.find(",")
+    right = str_engsentences[pos+2:]
+    
+    return  right.capitalize()
+
+print(remove_clause("It's being seen, but you aren't observing.") == "But you aren't observing.")
